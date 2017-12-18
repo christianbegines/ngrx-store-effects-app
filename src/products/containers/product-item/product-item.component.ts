6d9a326b818a09f96ae6464df03bd1806ad33133
christianbegines <christianbeginescarrasco@gmail.com>
2017-12-18 11:24:23 +0100

    curso finalizado

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -12,6 +12,7 @@ import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'product-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['product-item.component.scss'],
   template: `
     <div 
